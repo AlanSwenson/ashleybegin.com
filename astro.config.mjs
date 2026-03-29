9bd39197bc0a313c74255408e0b87e484d44fec4
AlanSwenson <alan@alanswenson.dev>
2026-03-29 09:01:19 -0500

    switch to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://ashleybegin.com',
+    adapter: cloudflare({
+        imageService: 'compile',
+    }),
     integrations: [
         tailwind(),
         sitemap({
             changefreq: 'weekly',
             priority: 0.7,
-            lastmod: new Date()
-        })
+            lastmod: new Date(),
+        }),
     ],
 });
